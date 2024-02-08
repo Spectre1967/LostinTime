88cbfbd3fddcce2b1676c6431a9ab3afe6f9b30f
VSwede <104148185+VSwede@users.noreply.github.com>
2024-02-08 08:54:29 +0100

    Justeringar till Pickup system

--- Lost in Time/Assets/Script/Player/PlayerInteractor.cs
+++ Lost in Time/Assets/Script/Player/PlayerInteractor.cs
@@ -14,18 +14,24 @@ interface IInteractable
 public class PlayerInteractor : MonoBehaviour
 {
     public Transform interactorSource;
+    public Camera playerCamera;
     public float interactionRange;
     [Space]
     public KeyCode interactionKey;
+    public float holdTime = 0.5f;
 
     [Space]
 
     public Sprite interactionKeyRegular;
     public Sprite interactionKeyHeld;
     [Space]
+    public RectTransform promptUI;
     public TMP_Text promptText;
     public Image promptImage;
+    public Image promptFill;
     public CanvasGroup promptCanvas;
+
+    float interactionTimer = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -36,18 +42,19 @@ public class PlayerInteractor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(interactionKey))
-        {
-            PerformInteraction();
-        }
-
         if (Input.GetKey(interactionKey))
         {
             promptImage.sprite = interactionKeyHeld;
+            PerformInteraction();
+
+            promptFill.fillAmount = Mathf.Clamp(interactionTimer / holdTime, 0, 1);
+
         }
         else
         {
             promptImage.sprite = interactionKeyRegular;
+            interactionTimer = 0;
+            promptFill.fillAmount = 0;
         }
 
         CheckForInteractions();
@@ -60,8 +67,13 @@ public class PlayerInteractor : MonoBehaviour
         {
             if(hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactObj))
             {
-                if(interactObj.IsInteractable())
-                    interactObj.Interact();
+                if (interactObj.IsInteractable())
+                {
+                    interactionTimer += Time.deltaTime;
+
+                    if(interactionTimer >= holdTime)
+                        interactObj.Interact();
+                }
             }
         }
     }
@@ -76,6 +88,8 @@ public class PlayerInteractor : MonoBehaviour
                 if (interactObj.IsInteractable())
                 {
                     promptText.text = interactObj.PromptText();
+                    Vector3 screenPos = playerCamera.WorldToScreenPoint(hitInfo.transform.position);
+                    promptUI.anchoredPosition = screenPos;
                     promptCanvas.alpha = 1;
                 }
                 else
