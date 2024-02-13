12693a4ac84a06ab4c14ecd046cbbb22ed7a59f0
Spectre1967 <104144711+Spectre1967@users.noreply.github.com>
2024-02-13 14:11:49 +0100

    navmeshagent katt

---
+++ Lost in Time/Assets/Script/Katt/KattMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;   
+
+public class KattMovement : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.position);
+
+        if(Getbuttondown("q"))
+        {
+
+        }
+    }
+}
